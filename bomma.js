47869225dd440ec7e2758f1f533ecccc24907cc7
Neerajv2000 <147934600+Neerajv2000@users.noreply.github.com>
2023-12-11 14:51:11 +0530

    Add files via upload

---
+++ bomma.js
@@ -0,0 +1,21 @@
+// let butt = document.querySelector("#but1");
+// butt.innerHTML="🌛"
+// butt.style.backgroundColor = "grey"
+// butt.style.padding = "3.5px"
+// butt.style.marginLeft = "95%"
+//  butt.addEventListener("click",function(){
+
+//    document.body.style.backgroundColor="black"
+//    document.body.style.color = "white"
+   
+//  })
+//  let butt2 = document.querySelector(".but2");
+// butt2.innerHTML="🌞"
+// butt2.style.padding = "3.5px"
+
+//  butt2.addEventListener("click",function(){
+
+//    document.body.style.backgroundColor="rgb(233, 233, 233)"
+//    document.body.style.color = "black"
+   
+//  })
